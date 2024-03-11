e0c771f2aff54051cdf64cb30d80b04a8fc14ec3
Jorge Bau <jbaugracia@gmail.com>
2024-03-11 20:16:58 +0100

    Fix/artifacts names version (#5)
    * Fixed command lib name
    
    * Fixed issues with parameters with short name only, also added some ANSI escape codes

--- lib/src/main/java/cli/ANSIEscapeCode.java
+++ lib/src/main/java/cli/ANSIEscapeCode.java
@@ -3,16 +3,32 @@ package cli;
 public enum ANSIEscapeCode {
 
 	
-	CLEAR(0), BLACK(30), RED(31), GREEN(32), YELLOW(33), BLUE(34), MAGENTA(35), CYAN(36), WHITE(37);
+	CLEAR(0),
+	BLACK(30),
+	RED(31),
+	GREEN(32),
+	YELLOW(33),
+	BLUE(34),
+	MAGENTA(35),
+	CYAN(36),
+	WHITE(37),
+	BRIGHT_BLUE(34, true),
+	BRIGHT_GREEN(32, true);
 	
 	private final int code;
-	
+	private boolean bright = false;
+
 	/**
 	 * Builds an escape code object
 	 * @param code Internal code representation
 	 */
 	private ANSIEscapeCode(int code) {
 		this.code = code;
+	}
+
+	private ANSIEscapeCode(int code, Boolean bright) {
+		this(code);
+		this.bright = bright;
 	}
 	
 	/**
@@ -22,6 +38,7 @@ public enum ANSIEscapeCode {
 	private String getCode() {
 		return Integer.toString(this.code);
 	}
+	private Boolean isBright() { return this.bright; }
 	
 	/**
 	 * Returns a string 'painted' in the specified color.
@@ -30,6 +47,6 @@ public enum ANSIEscapeCode {
 	 * @return Painted string
 	 */
 	public static String paint(String s, ANSIEscapeCode code) {
-		return (char)27 + "[" + code.getCode() + "m" + s + (char)27 + "[" + WHITE.getCode() + "m"; 
+		return (char)27 + "[" + code.getCode() + (code.isBright() ? ";1" : "") + "m" + s + (char)27 + "[0m";
 	}
 }
