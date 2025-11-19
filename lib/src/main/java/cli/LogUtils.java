5f53c658e945a72bf9f4bee5a9645079275e18dc
Jorge Bau <jbaugracia@gmail.com>
2025-11-19 18:57:34 +0100

    v1.11.0 (#11)
    * Logging improvements: keeping initial spec, simplify to define custom loggers in client apps
    
    * Documentation improvements
    
    * Typo
    
    * Doc improvements
    
    * Updated quite old commons-lang dependency
    
    * Fixed default logger behavior
    
    * We didn't actually need reflections in the library pom
    
    * We did not need slf4j either in the library
    
    * Closed version, ignored vscode-related files

--- lib/src/main/java/cli/LogUtils.java
+++ lib/src/main/java/cli/LogUtils.java
@@ -11,22 +11,40 @@ import static java.util.logging.Logger.getLogger;
 
 public class LogUtils {
 
+    private static final String DEFAULT_LOGGING_CONFIGURATION = "/cli-logging.properties";
+    private static final String CUSTOM_LOGGING_CONFIGURATION = "/custom-cli-logging.properties";
+
     private static final String ENV_VAR = "CLI_LOG_LEVEL";
 
     public LogUtils() {
-        try(InputStream stream = LogUtils.class.getResourceAsStream("/cli-logging.properties")) {
+        try(InputStream stream = getLoggingConfiguration()) {
             getLogManager().readConfiguration(stream);
             // We can override log level by the env variable CLI_LOG_LEVEL
             // It can take any value in the java util logging Level enumeration
 
             var level = getLogLevelFromEnvironment();
-            getLogger("cli").setLevel(level);
-            // The following message will only be shown if log is fine enough
-            getLogger("cli").log(
-                Level.FINEST,
-                String.format("Overridden log level to %s by env var %s", level, ENV_VAR)
-            );
+            if (level != null) {
+                getLogger("cli").setLevel(level);
+                // The following message will only be shown if log is fine enough
+                getLogger("cli").log(
+                        Level.FINEST,
+                        String.format("Overridden log level to %s by env var %s", level, ENV_VAR)
+                );
+            } else {
+                if (getLogger("cli").getLevel() == null) {
+                    // If the client app did not define a log level, we assume that the default logger is meant to let
+                    //  everything pass, and we rely on each specific handler level
+                    getLogger("cli").setLevel(Level.ALL);
+                }
+            }
         } catch (IOException ioe) { ioe.printStackTrace(); }
+    }
+
+    private InputStream getLoggingConfiguration() {
+        InputStream ret = null;
+        ret = LogUtils.class.getResourceAsStream(CUSTOM_LOGGING_CONFIGURATION);
+        if (ret == null) ret = LogUtils.class.getResourceAsStream(DEFAULT_LOGGING_CONFIGURATION);
+        return ret;
     }
 
     public static Logger getDefaultLogger() {
@@ -37,6 +55,6 @@ public class LogUtils {
         var value = System.getenv(ENV_VAR);
         return (value != null && !value.trim().isEmpty())
             ? Level.parse(value.toUpperCase())
-            : SEVERE;
+            : null;
     }
 }
